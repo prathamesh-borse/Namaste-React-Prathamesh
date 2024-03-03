2cf6c1f51e353655e5fb817cce2a3a153cb77086
Prathamesh Borse <prathameshborse.official@gmail.com>
2024-03-03 17:26:50 +0530

    Done with Episode-02 and Started with Assignment Episode-02

---
+++ Codes/Igniting_Our_App/App.js
@@ -0,0 +1,42 @@
+/*
+
+<div id="parent">
+    <div id="child">
+        <h1></h1>
+    </div>
+</div>
+
+*/
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "This is Namaste React 🚀"),
+    React.createElement("h2", {}, "I'm an H2 Tag"),
+  ]),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm an H1 Tag"),
+    React.createElement("h2", {}, "I'm an H2 Tag"),
+  ]),
+]);
+
+console.log("Parent: ", parent);
+
+const heading = React.createElement(
+  "h1",
+  {
+    id: "heading",
+  },
+  "Hello From React!"
+);
+// {} = where we specify the attributes our element.
+
+// heading = is the react javascript object in which we have props and childern of react
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
+// render = basically getting the heading react javascript object and converting it into the h1 heading tag
+// and adding inside the DOM tree of html inside the webpage.
